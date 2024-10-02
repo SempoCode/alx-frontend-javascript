727e6edd513967aba1c8ced3f9b77b0fbb50d83a
SempoCode <sempo.code@gmail.com>
2024-10-02 11:31:34 +0000

    4. Simple promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
 // 4-user-promise.js
 export default function signUpUser(firstName, lastName) {
-	return Promise.resolve({ firstName, lastName });
+  return Promise.resolve({ firstName, lastNam });
 }
